9e4add3a5376ea691a09b1e3db16f224eecc3558
Fabian <fabdan-8@student.ltu.se>
2023-04-06 13:59:15 +0200

    spinning proboscis monkey

--- Global.h
+++ Global.h
@@ -6,4 +6,24 @@
 
 #include "Game.h"
 
+//SDL - temporary
+#define SDL_MAIN_HANDLED //must be defined
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_net.h>
+
+//SDL-specific objects
+SDL_Renderer* renderer = nullptr;
+SDL_Window* window = nullptr;
+SDL_Event mainevent;
+Mix_Music* music = nullptr;
+SDL_Texture* debug_texture = nullptr;
+
 Game game;//game object
+
+//screen and mouse variables
+int screenW;
+int screenH;
+int mouseX;
+int mouseY;
