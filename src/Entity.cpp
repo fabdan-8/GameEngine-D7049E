84f2502347572c4a2563040f1c0447fe88365c2a
Tricard <adelie@PC-linux>
2023-05-01 00:52:37 +0200

    physics and rigidbody done, no tests

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -2,3 +2,13 @@
 
 Entity::Entity(int entityId) : entityId(entityId) {
 }
+
+Ogre::Vector3 Entity::getSize(){
+    Ogre::Vector3 vector;
+    return vector;
+}
+
+Ogre::Real Entity::getMass(){
+    Ogre::Real real;
+    return real;
+}
