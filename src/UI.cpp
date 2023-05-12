6956e344635aaa885d6357d2ac4eb60d6af4e3ed
fabdan-8 <fabdan-8@student.ltu.se>
2023-05-12 14:11:58 +0200

    Adélie's changes

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,43 +1,43 @@
 #include "UI.h"
 
-#include "Game.h"
-
-#include "Ogre.h"
-#include "OgreApplicationContext.h"
-#include "OgreOverlay.h"
-#include "OgreOverlayManager.h"
-
-extern Game game;
-
-void UI::Load() {
-    // create material and texture
-    Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().create("MyImageMaterial", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-    Ogre::TexturePtr texture = Ogre::TextureManager::getSingleton().load("Skeleton.bmp", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-    material->getTechnique(0)->getPass(0)->createTextureUnitState()->setTextureName("Skeleton.bmp");
-
-    // create overlay container and element
-    //Ogre::Overlay* container = new Ogre::Overlay;
-    container = (Ogre::OverlayContainer*)(Ogre::OverlayManager::getSingleton().createOverlayElement("Panel", "MyImageContainer"));//sus
-    element = Ogre::OverlayManager::getSingleton().createOverlayElement("Image", "MyImageElement");
-    element->setMaterialName("MyImageMaterial");
-    container->addChild(element);
-
-    // set position and size of overlay container and element
-    container->setPosition(100, 100);
-    container->setDimensions(200, 200);
-    element->setPosition(0, 0);
-    element->setDimensions(200, 200);
-
-    // show overlay container
-    Ogre::Overlay* overlay = Ogre::OverlayManager::getSingleton().create("MyOverlay");
-    overlay->add2D(container);
-    overlay->show();
-}
-
-void UI::Update() {
-    Ogre::Camera* camera = game.GetCamera();
-    Ogre::Viewport* viewport = camera->getViewport();
-    Ogre::Real screenX = viewport->getActualWidth() * 0.5f;
-    Ogre::Real screenY = viewport->getActualHeight() * 0.5f;
-    container->setPosition(screenX - 100, screenY - 100);
-}
+//#include "Game.h"
+//
+//#include "Ogre.h"
+//#include "OgreApplicationContext.h"
+//#include "OgreOverlay.h"
+//#include "OgreOverlayManager.h"
+//
+//extern Game game;
+//
+//void UI::Load() {
+//    // create material and texture
+//    Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().create("MyImageMaterial", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+//    Ogre::TexturePtr texture = Ogre::TextureManager::getSingleton().load("Skeleton.bmp", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+//    material->getTechnique(0)->getPass(0)->createTextureUnitState()->setTextureName("Skeleton.bmp");
+//
+//    // create overlay container and element
+//    //Ogre::Overlay* container = new Ogre::Overlay;
+//    container = (Ogre::OverlayContainer*)(Ogre::OverlayManager::getSingleton().createOverlayElement("Panel", "MyImageContainer"));//sus
+//    element = Ogre::OverlayManager::getSingleton().createOverlayElement("Image", "MyImageElement");
+//    element->setMaterialName("MyImageMaterial");
+//    container->addChild(element);
+//
+//    // set position and size of overlay container and element
+//    container->setPosition(100, 100);
+//    container->setDimensions(200, 200);
+//    element->setPosition(0, 0);
+//    element->setDimensions(200, 200);
+//
+//    // show overlay container
+//    Ogre::Overlay* overlay = Ogre::OverlayManager::getSingleton().create("MyOverlay");
+//    overlay->add2D(container);
+//    overlay->show();
+//}
+//
+//void UI::Update() {
+//    Ogre::Camera* camera = game.GetCamera();
+//    Ogre::Viewport* viewport = camera->getViewport();
+//    Ogre::Real screenX = viewport->getActualWidth() * 0.5f;
+//    Ogre::Real screenY = viewport->getActualHeight() * 0.5f;
+//    container->setPosition(screenX - 100, screenY - 100);
+//}
--- src/UI.h
+++ src/UI.h
@@ -1,13 +1,41 @@
 #pragma once
 
-#include "OgreOverlayContainer.h"
-#include "OgreOverlayElement.h"
+#include <OgreOverlayManager.h>
 
-class UI {
-public:
-	void Load();
-	void Update();
-private:
-	Ogre::OverlayContainer* container = nullptr;
-	Ogre::OverlayElement* element = nullptr;
+class UI { 
+
+    //Create a panel
+    Ogre::OverlayContainer* UI::createPanel(std::string panel_name, float position_x, float position_y, float height, float width, std::string material_name) { 
+        Ogre::OverlayManager& overlayManager = Ogre::OverlayManager::getSingleton();
+        Ogre::OverlayContainer* panel = static_cast<Ogre::OverlayContainer*>( overlayManager.createOverlayElement( "Panel", panel_name ) );
+        panel->setPosition( position_x, position_y);
+        panel->setDimensions( height, width );
+        panel->setMaterialName( material_name );
+        return panel;
+    }
+
+    //Create an overlay
+    Ogre::Overlay* UI::createOverlay(std::string overlay_name) {
+        return overlayManager.create( overlay_name );
+    }
+
+    //Create a text
+    Ogre::TextAreaOverlayElement* UI::createText() {
+        return static_cast<Ogre::TextAreaOverlayElement*>(overlayManager.createOverlayElement("TextArea", "TextView"));
+    }
+
+    //Add the panel to the overlay
+    void UI::PanelToOverlay(Ogre::Overlay* overlay, Ogre::OverlayContainer* panel) {
+        overlay->add2D( panel );
+    }
+    
+
+    //Add text to panel
+    void UI::TextToPanel(Ogre::OverlayContainer* panel, Ogre::TextAreaOverlayElement text) {
+        panel->addChild(text);
+    }    
+
+    // Show the overlay
+    //overlay->show();
+
 };
