aaab4af5a43dae4cb55ed7171f7fe3846f7bd6da
Tricard <adelie@PC-linux>
2023-05-12 16:48:28 +0200

    fixing physics

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -1,24 +1,24 @@
 #include "Physics.h"
 
 Physics::Physics(float gravityX, float gravityY, float gravityZ ) {
-    //collision configuration specifies the type of collision detection algorithm to use
-    collisionConfiguration = new btDefaultCollisionConfiguration();
+    // //collision configuration specifies the type of collision detection algorithm to use
+    // collisionConfiguration = new btDefaultCollisionConfiguration();
     
-    //dispatcher is responsible for sending collision detection events to the correct objects 
-    dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    // //dispatcher is responsible for sending collision detection events to the correct objects 
+    // dispatcher = new btCollisionDispatcher(collisionConfiguration);
     
-    //broadphase algorithm create a list of possible collisions for the dispatcher to check
-    broadphase = new btDbvtBroadphase();
+    // //broadphase algorithm create a list of possible collisions for the dispatcher to check
+    // broadphase = new btDbvtBroadphase();
 
-    //solver resolve collisions & apply constraints
-    solver = new btSequentialImpulseConstraintSolver();
+    // //solver resolve collisions & apply constraints
+    // solver = new btSequentialImpulseConstraintSolver();
     
-    //dynamics world contains all the objects in the simulation & updates them each frame
-    //simulates gravity and apply forces to objects
-    dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+    // //dynamics world contains all the objects in the simulation & updates them each frame
+    // //simulates gravity and apply forces to objects
+    // dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
     
-    //set gravity (0, -9.81, 0) for Earth
-    dynamicsWorld->setGravity(btVector3(gravityX, gravityY, gravityZ));
+    // //set gravity (0, -9.81, 0) for Earth
+    // dynamicsWorld->setGravity(btVector3(gravityX, gravityY, gravityZ));
 }
 
 Physics::Physics() {
@@ -99,22 +99,23 @@ btRigidBody* Physics::createRigidBody(Ogre::SceneNode* node, Entity *entity) {
 void Physics::update(int timeSinceLastFrame) {
     dynamicsWorld->stepSimulation(timeSinceLastFrame * 0.001f, 10);
     btRigidBody *rb;
+    for(auto const& iterator : rigidBodies){
+        if(iterator.second){
+            // Update renderer
+            // Ogre::SceneNode *node = static_cast<Ogre::SceneNode *>(iterator.second->getUserPointer());
+            Ogre::SceneNode *node = iterator.first->getNode();
+            rb = iterator.second;
 
-    for(std::map<Entity*, btRigidBody*>::iterator it = rigidBodies.begin(); it != rigidBodies.end(); ++it) {
-        // Update renderer
-        Ogre::SceneNode *node = static_cast<Ogre::SceneNode *>(it->second->getUserPointer());
-        rb = it->second;
-
-        // Set position
-        btVector3 Point = rb->getCenterOfMassPosition();
-        node->setPosition(Ogre::Vector3((float)Point[0], (float)Point[1], (float)Point[2]));
-        // Convert the bullet Quaternion to an Ogre quaternion
-        btQuaternion btq = rb->getOrientation();
-        Ogre::Quaternion quart = Ogre::Quaternion(btq.w(),btq.x(),btq.y(),btq.z());
-        // use the quaternion with setOrientation
-        node->setOrientation(quart);
+            // Set position
+            btVector3 Point = rb->getCenterOfMassPosition();
+            node->setPosition(Ogre::Vector3((float)Point[0], (float)Point[1], (float)Point[2]));
+            // Convert the bullet Quaternion to an Ogre quaternion
+            btQuaternion btq = rb->getOrientation();
+            Ogre::Quaternion quart = Ogre::Quaternion(btq.w(),btq.x(),btq.y(),btq.z());
+            // use the quaternion with setOrientation
+            node->setOrientation(quart);
+        }
     }
-    
 }
 
 void Physics::SetGravity(float gravityX, float gravityY, float gravityZ) {
