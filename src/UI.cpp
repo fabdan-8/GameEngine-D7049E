45e6c7c8ed43a7464c5ce01d37fcc2c9216b9142
Tricard <adelie@PC-linux>
2023-05-13 18:41:17 +0200

    panel textarea and overlay not working...

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,14 +1,69 @@
 #include "UI.h"
 
-//#include "Game.h"
-//
-//#include "Ogre.h"
-//#include "OgreApplicationContext.h"
-//#include "OgreOverlay.h"
-//#include "OgreOverlayManager.h"
-//
-//extern Game game;
-//
+#include "Ogre.h"
+#include "OgreApplicationContext.h"
+#include "OgreFontManager.h"
+#include <algorithm>
+
+
+//Create a panel
+Ogre::OverlayContainer* UI::createPanel(std::string panel_name, float position_x, float position_y, float height, float width, std::string material_name) { 
+    Ogre::OverlayManager& overlayManager = Ogre::OverlayManager::getSingleton();
+    //Create a panel
+    Ogre::OverlayContainer* panel = static_cast<Ogre::OverlayContainer*>(overlayManager.createOverlayElement("Panel", panel_name));
+    panel->setMetricsMode(Ogre::GMM_PIXELS);
+    panel->setPosition(position_x, position_y); 
+    panel->setDimensions(height, width);
+    //Optional background material
+    panel->setMaterialName(material_name); 
+    return panel;
+}
+
+//Create an overlay
+Ogre::Overlay* UI::createOverlay(std::string overlay_name) {
+    return overlayManager.create( overlay_name );
+}
+
+//Create a text
+Ogre::TextAreaOverlayElement* UI::createText(std::string text_name, std::string text, std::string font_name, float size, float position_x, float position_y, float height, float width, std::string bottom_color, std::string top_color) {
+    // Create a text area
+    Ogre::TextAreaOverlayElement* textArea = static_cast<Ogre::TextAreaOverlayElement*>(overlayManager.createOverlayElement("TextArea", text_name));
+    textArea->setMetricsMode(Ogre::GMM_PIXELS);
+    textArea->setPosition(position_x,position_y);
+    textArea->setDimensions(height,widht);
+    textArea->setCaption(text);
+    textArea->setCharHeight(size); 
+    textArea->setFontName(font_name);
+    if(bottom_color.tolower() == "black"){
+        textArea->setColourBottom(Ogre::ColorValue(0.0, 0.0, 0.0, 1.0));
+    }
+    else if(bottom_color.tolower() == "white"){
+        textArea->setColourBottom(Ogre::ColorValue(1.0, 1.0, 1.0, 1.0));        
+    }
+    if(top_color.tolower() == "black"){
+        textArea->setColourTop(Ogre::ColorValue(0.0, 0.0, 0.0, 1.0));
+    }
+    else if(top_color.tolower() == "white"){
+        textArea->setColourTop(Ogre::ColorValue(1.0, 1.0, 1.0, 1.0));        
+    }
+    return textArea;
+}
+
+//Add the panel to the overlay
+void UI::PanelToOverlay(Ogre::Overlay* overlay, Ogre::OverlayContainer* panel) {
+    overlay->add2D( panel );
+}
+    
+//Add text to panel
+void UI::TextToPanel(Ogre::OverlayContainer* panel, Ogre::TextAreaOverlayElement text) {
+    panel->addChild(text);
+} 
+
+//Show the overlay
+void ShowOverlay(Ogre::Overlay* overlay){
+    overlay->show();
+}
+
 //void UI::Load() {
 //    // create material and texture
 //    Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().create("MyImageMaterial", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
--- src/UI.h
+++ src/UI.h
@@ -1,41 +1,28 @@
 #pragma once
 
-#include <OgreOverlayManager.h>
+#include "OgreOverlayManager.h"
+#include "OgreTextAreaOverlayElement.h"
+#include "OgreColourValue.h"
+#include <string>
 
 class UI { 
 
-    ////Create a panel
-    //Ogre::OverlayContainer* UI::createPanel(std::string panel_name, float position_x, float position_y, float height, float width, std::string material_name) { 
-    //    Ogre::OverlayManager& overlayManager = Ogre::OverlayManager::getSingleton();
-    //    Ogre::OverlayContainer* panel = static_cast<Ogre::OverlayContainer*>( overlayManager.createOverlayElement( "Panel", panel_name ) );
-    //    panel->setPosition( position_x, position_y);
-    //    panel->setDimensions( height, width );
-    //    panel->setMaterialName( material_name );
-    //    return panel;
-    //}
+    //Create a panel
+    Ogre::OverlayContainer* createPanel(std::string panel_name, float position_x, float position_y, float height, float width, std::string material_name);
 
-    ////Create an overlay
-    //Ogre::Overlay* UI::createOverlay(std::string overlay_name) {
-    //    return overlayManager.create( overlay_name );
-    //}
+    //Create a text
+    Ogre::TextAreaOverlayElement* createText(std::string text_name, std::string text, std::string font_name, float size, float position_x, float position_y, float height, float width, std::string bottom_color, std::string top_color);
 
-    ////Create a text
-    //Ogre::TextAreaOverlayElement* UI::createText() {
-    //    return static_cast<Ogre::TextAreaOverlayElement*>(overlayManager.createOverlayElement("TextArea", "TextView"));
-    //}
+    //Create an overlay
+    Ogre::Overlay* createOverlay(std::string overlay_name);
 
-    ////Add the panel to the overlay
-    //void UI::PanelToOverlay(Ogre::Overlay* overlay, Ogre::OverlayContainer* panel) {
-    //    overlay->add2D( panel );
-    //}
-    //
+    //Add the panel to the overlay
+    void PanelToOverlay(Ogre::Overlay* overlay, Ogre::OverlayContainer* panel);
 
-    ////Add text to panel
-    //void UI::TextToPanel(Ogre::OverlayContainer* panel, Ogre::TextAreaOverlayElement text) {
-    //    panel->addChild(text);
-    //}    
+    //Add text to panel
+    void TextToPanel(Ogre::OverlayContainer* panel, Ogre::TextAreaOverlayElement text);
 
-    //// Show the overlay
-    ////overlay->show();
+    // Show the overlay
+    void ShowOverlay(Ogre::Overlay* overlay);
 
 };
